01853318602dbd5a2e2ec2f130b1ed35d131f766
Egor Dudyrev <egor.dudyrev@yandex.ru>
2019-02-10 00:12:19 +0300

    Complete HW5

--- docker_compose/storage_backend/src/agg.js
+++ docker_compose/storage_backend/src/agg.js
@@ -1,24 +1,30 @@
 /*
         Написать запрос, который выводит общее число тегов
 */
-print("tags count: ", 'расчёт количества тегов');
+print("tags count: ", db.tags.count());
 /*
         Добавляем фильтрацию: считаем только количество тегов woman
 */
-print("woman tags count: ", 'расчёт количества тегов woman');
+print("woman tags count: ",  db.tags.find({name:'woman'}).count());
 /*
         Очень сложный запрос: используем группировку данных посчитать количество вхождений для каждого тега
         и напечатать top-3 самых популярных
 */
 
+db.tags.aggregate([{$group: {_id: "$name", tag_count:{$sum:1}}},{$sort:{tag_count:-1}},{$limit:3}])
+{ "_id" : "woman director", "tag_count" : 3115 }
+{ "_id" : "independent film", "tag_count" : 1930 }
+{ "_id" : "murder", "tag_count" : 1308 }
+
+
 printjson(
         db.tags.aggregate([
                 {"$group": {
-                                "--": "--",
-                                "--": "--"
+                                "_id": "$name",
+                                "tag_count": {"$sum":1}
                            }
                 },
-                {"тут модификатор sort по полю с аггрегацией"},
+                {"$sort":{"tag_count":-1}},
                 {$limit: 3}
         ])['_batch']
 );
